2126805f2756b6a6e23c71bbf41f5757198eab86
NDreKru <ndrekrukru@gmail.com>
2021-05-16 20:04:02 +0500

    3. Панграмма

---
+++ src/collections/Pangram.java
@@ -0,0 +1,20 @@
+package collections;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Set;
+
+public class Pangram {
+    public static boolean checkString(String s) {
+        String formattedStr = s.toLowerCase();
+        formattedStr = formattedStr.replace(" ", "");
+        char[] arr = formattedStr.toCharArray();
+        Set<Character> alphabet = new HashSet<>();
+        for (char c : arr) {
+            alphabet.add(c);
+        }
+        return alphabet.size() == 26;
+    }
+}
